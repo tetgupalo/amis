c869f0a39e9abad45d8b5c74c1865ad62392e3ca
PashaKharambura <harambura69@mail.ru>
2017-12-22 12:09:08 +0200

    Update Populate.sql

--- students/km41/Kharambura/Code/Populate.sql
+++ students/km41/Kharambura/Code/Populate.sql
@@ -1,11 +1,7 @@
-
 INSERT INTO "Roles" ("role_name", "Description") 
 VALUES ('User', 'Can buy tickets');
 INSERT INTO "Roles" ("role_name", "Description")
 VALUES ('VipUser', 'Can buy tickets with discount');
-INSERT INTO "Roles" ("role_name", "Description")
-VALUES ('Admin', 'Manage DB');
-
 
 INSERT INTO "USERS" ("user_email", "role_name", "user_password", "user_firstname", "user_lastname", "user_cardnumber", "user_adress")
 VALUES ('oleg.harambura@gmail.com', 'User', '111111', 'Oleg', 'Kharambura', '000000001','Kovalskiy 5, Kyiv');
@@ -14,22 +10,19 @@ VALUES ('galyna.harambura@gmail.com', 'User', '222222', 'Galyna', 'Kharambura', 
 INSERT INTO "USERS" ("user_email", "role_name", "user_password", "user_firstname", "user_lastname", "user_cardnumber", "user_adress")
 VALUES ('pasha.harambura@gmail.com', 'User', '333333', 'Pavlo', 'Kharambura', '000000003','Kovalskiy 5, Kyiv');
 
-
-INSERT INTO "TICKET" ("ticket_type", "ticket_price", "transport_company") 
+INSERT INTO "TICKET" ("ticket_type", "ticket_price", "transport_company", "AMOUNT") 
 VALUES ('Bus', 6, 'KyivPasTrans');
-INSERT INTO "TICKET" ("ticket_type", "ticket_price", "transport_company") 
+INSERT INTO "TICKET" ("ticket_type", "ticket_price", "transport_company, "AMOUNT") 
 VALUES ('Tramvay', 4, 'KyivPasTrans');
-INSERT INTO "TICKET" ("ticket_type", "ticket_price", "transport_company") 
+INSERT INTO "TICKET" ("ticket_type", "ticket_price", "transport_company", "AMOUNT") 
 VALUES ('Metro', 5, 'KyivPasTrans');
-INSERT INTO "TICKET" ("ticket_type", "ticket_price", "transport_company") 
+INSERT INTO "TICKET" ("ticket_type", "ticket_price", "transport_company", "AMOUNT") 
 VALUES ('TroleybasBus', 6, 'KyivPasTrans');
-
 
 INSERT INTO "ORDERS" ("user_email", "order_date", "order_id")
 VALUES ('pasha.harambura@gmail.com', (TO_DATE('2017/11/11 21:02:44', 'yyyy/mm/dd hh24:mi:ss')), 000000001);
 INSERT INTO "ORDERS" ("user_email", "order_date", "order_id")
 VALUES ('oleg.harambura@gmail.com', (TO_DATE('2017/12/11 12:02:44', 'yyyy/mm/dd hh24:mi:ss')), 000000002);
-
 
 INSERT INTO "ORDERITEM" ("ticket_type", "user_email", "order_id", "tickets_quantity") 
 VALUES ('Bus', 'pasha.harambura@gmail.com', 000000001, 5);
@@ -37,7 +30,3 @@ INSERT INTO "ORDERITEM" ("ticket_type", "user_email", "order_id", "tickets_quant
 VALUES ('Metro', 'pasha.harambura@gmail.com', 000000001, 10);
 INSERT INTO "ORDERITEM" ("ticket_type", "user_email", "order_id", "tickets_quantity") 
 VALUES ('Tramvay', 'oleg.harambura@gmail.com', 000000002, 20);
-
-alter table "USER"
-  add constraint check_email
-  check ( REGEXP_LIKE (user_email, '[a-z0-9._]+@[a-z0-9._]+\.[a-z]{2,4}'));
